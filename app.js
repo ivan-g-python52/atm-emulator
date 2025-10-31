79892dfcc5b8757dc42848d16531ed61f0a2ccdd
Ivan <krya.krya.krya_yes_i_am@mail.ru>
2025-10-31 19:19:14 +0300

    feat: add show balance

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 'use strict';
+let balance = 10000;
+
+while (true) {
+    const menu = `
+    Добро пожаловать в банкомат!
+    Ваш выбор:
+    1. Показать Баланс
+    2. Снять средства
+    3. Пополнить счёт
+    4. Выход
+    `;
+
+    const choice = prompt(menu);
+
+    switch (choice) {
+        case '1':
+            alert(`Ваш баланс: ${balance} $`);
+            break;
+    }
+}
